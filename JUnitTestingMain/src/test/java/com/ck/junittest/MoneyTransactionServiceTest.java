1088d4f3f79aca4403225978b24706be27613b75
kongaman <christian.kranz@gmail.com>
2024-04-25 19:15:14 +0200

    Abschnitt 3 Video 7
    grouped assertions example

--- JUnitTestingMain/src/test/java/com/ck/junittest/MoneyTransactionServiceTest.java
+++ JUnitTestingMain/src/test/java/com/ck/junittest/MoneyTransactionServiceTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Money Transaction Service Test")
 class MoneyTransactionServiceTest {
 
 	private static final String MONEY_AMOUNT_EXCEPTION_MSG = "Money amount should be greater than 0";
@@ -16,64 +18,62 @@ class MoneyTransactionServiceTest {
 	private static final double ZERO_MONEY_AMOUNT = 0;
 	private static final double MORE_THAN_RANDOM_MONEY_AMOUNT = 200;
 	private static final double NEGATIVE_MONEY_AMOUNT = -1;
-	
-	
+
 	private MoneyTransactionService testInstance;
-	
+
 //	@Before 			// in JUnit 4
-	@BeforeEach 		// in JUnit 5
+	@BeforeEach // in JUnit 5
 	void setUp() {
 		testInstance = new MoneyTransactionService();
 	}
-	
+
 //	@After 			// in JUnit 4
-	@AfterEach		// in JUnit 5
+	@AfterEach // in JUnit 5
 	void tearDown() {
 		// this method will be executed after each test method
 	}
-	
+
 //	@BeforeClass	// in JUnit 4 and static method!
-	@BeforeAll		// in JUnit 5 and static method!
+	@BeforeAll // in JUnit 5 and static method!
 	static void beforeAll() {
 		// this method will be executed before all tests
 	}
-	
+
 //	@AfterClass // in JUnit 4
-	@AfterAll	// in JUnit 5
+	@AfterAll // in JUnit 5
 	static void afterAll() {
-		
+
 	}
-	
+
 	@Test
+	@DisplayName("Verify money transaction from one account to another")
 //	void transferMoneyTest() {
 	void shouldTransferMoneyFromOneAccountToAnother() {
 		// GIVEN
 		var account1 = new Account(RANDOM_MONEY_AMOUNT);
 		var account2 = new Account(ZERO_MONEY_AMOUNT);
-		
+
 		// WHEN
 		testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT);
-		
+
 		// THEN
 		assertEquals(ZERO_MONEY_AMOUNT, account1.getMoneyAmount());
 		assertEquals(RANDOM_MONEY_AMOUNT, account2.getMoneyAmount());
 	}
-	
 
 	@Test
 	void shouldThrowExceptionIfAccountFromIsNull() {
 		// GIVEN
 		Account account1 = null;
 		Account account2 = new Account(RANDOM_MONEY_AMOUNT);
-		
+
 		// WHEN & THEN
-		var exception = assertThrows(IllegalArgumentException.class, () -> 
-			testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT)
-		);
-		
+		var exception = assertThrows(IllegalArgumentException.class,
+				() -> testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT));
+
 		assertEquals(ACCOUNT_EXCEPTION_MSG, exception.getMessage());
 	}
-	
+
 //	@org.junit.Test(expected = IllegalArgumentException.class)
 //	public void shouldThrowExceptionIfAccountFromIsNull2() {
 //		// GIVEN
@@ -84,48 +84,45 @@ class MoneyTransactionServiceTest {
 //		// WHEN & THEN
 //		testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT);
 //	}
-	
+
 	@Test
 	void shouldThrowExceptionIfAccountToIsNull() {
 		// GIVEN
 		Account account1 = new Account(RANDOM_MONEY_AMOUNT);
 		Account account2 = null;
-		
+
 		// WHEN & THEN
-		var exception = assertThrows(IllegalArgumentException.class, () -> 
-			testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT)
-		);
-		
+		var exception = assertThrows(IllegalArgumentException.class,
+				() -> testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT));
+
 		assertEquals(ACCOUNT_EXCEPTION_MSG, exception.getMessage());
 	}
-	
+
 	@Test
 	void shouldThrowNotEnoughMoneyExceptionWhenTransferMoreMoney() {
 		// GIVEN
 		var account1 = new Account(RANDOM_MONEY_AMOUNT);
 		var account2 = new Account(ZERO_MONEY_AMOUNT);
-		
+
 		// WHEN
-		assertThrows(NotEnoughMoneyException.class, () -> 
-			testInstance.transferMoney(account1, account2, MORE_THAN_RANDOM_MONEY_AMOUNT)
-		);
-		
+		assertThrows(NotEnoughMoneyException.class,
+				() -> testInstance.transferMoney(account1, account2, MORE_THAN_RANDOM_MONEY_AMOUNT));
+
 	}
-	
+
 	@Test
 	void shouldThrowExcpetionWhenTransferNegativeAmount() {
 		// GIVEN
 		var account1 = new Account();
 		var account2 = new Account();
-		
+
 		// WHEN
-		var exception = assertThrows(IllegalArgumentException.class, () -> 
-					testInstance.transferMoney(account1, account2, NEGATIVE_MONEY_AMOUNT)
-				);
-		
+		var exception = assertThrows(IllegalArgumentException.class,
+				() -> testInstance.transferMoney(account1, account2, NEGATIVE_MONEY_AMOUNT));
+
 		assertEquals(MONEY_AMOUNT_EXCEPTION_MSG, exception.getMessage());
 	}
-	
+
 	@Test
 	void shouldThrowExcpetionWhenTransferZeroMoneyAmount() {
 		// GIVEN
@@ -133,11 +130,29 @@ class MoneyTransactionServiceTest {
 		var account2 = new Account();
 
 		// WHEN
-		var exception = assertThrows(IllegalArgumentException.class, () -> 
-					testInstance.transferMoney(account1, account2, ZERO_MONEY_AMOUNT)
-				);
-		
+		var exception = assertThrows(IllegalArgumentException.class,
+				() -> testInstance.transferMoney(account1, account2, ZERO_MONEY_AMOUNT));
+
 		assertEquals(MONEY_AMOUNT_EXCEPTION_MSG, exception.getMessage());
+	}
+
+	@Test
+	void groupedAssertionsExamples() {
+		// In a grouped assertion all assertions are executed and all failures will be
+		// reported together.
+
+		// GIVEN
+		var account1 = new Account(RANDOM_MONEY_AMOUNT);
+		var account2 = new Account(ZERO_MONEY_AMOUNT);
+		
+		// WHEN
+		testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT);
+		
+		//THEN
+		assertAll("money transaction", // <-- Text im Fehlerfall
+				() -> assertEquals(ZERO_MONEY_AMOUNT, account1.getMoneyAmount()),
+				() -> assertEquals(RANDOM_MONEY_AMOUNT, account2.getMoneyAmount())
+				);
 	}
 
 }
