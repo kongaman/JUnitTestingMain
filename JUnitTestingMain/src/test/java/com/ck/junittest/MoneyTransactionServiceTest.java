e0383b974d8f04242e4cbb7ced5a6f4f1022ec76
kongaman <christian.kranz@gmail.com>
2024-04-26 16:59:54 +0200

    Abschnitt 3 Video 7
    advanced tests examples

--- JUnitTestingMain/src/test/java/com/ck/junittest/MoneyTransactionServiceTest.java
+++ JUnitTestingMain/src/test/java/com/ck/junittest/MoneyTransactionServiceTest.java
@@ -1,6 +1,16 @@
 package com.ck.junittest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import java.time.Duration;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -8,6 +18,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Money Transaction Service Test")
 class MoneyTransactionServiceTest {
@@ -80,7 +98,7 @@ class MoneyTransactionServiceTest {
 //		Account account1 = null;
 //		Account account2 = new Account(RANDOM_MONEY_AMOUNT);
 //		testInstance = new MoneyTransactionService();
-//		
+//
 //		// WHEN & THEN
 //		testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT);
 //	}
@@ -144,15 +162,117 @@ class MoneyTransactionServiceTest {
 		// GIVEN
 		var account1 = new Account(RANDOM_MONEY_AMOUNT);
 		var account2 = new Account(ZERO_MONEY_AMOUNT);
-		
+
 		// WHEN
 		testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT);
-		
-		//THEN
-		assertAll("money transaction", // <-- Text im Fehlerfall
+
+		// THEN
+		assertAll("money transaction (grouped)", // <-- Text im Fehlerfall
 				() -> assertEquals(ZERO_MONEY_AMOUNT, account1.getMoneyAmount()),
-				() -> assertEquals(RANDOM_MONEY_AMOUNT, account2.getMoneyAmount())
-				);
+				() -> assertEquals(RANDOM_MONEY_AMOUNT, account2.getMoneyAmount()));
+	}
+
+	@Test
+	void dependentAssertionsExample() {
+		// Within a code block, if an assertion fails the subsequent code in the same
+		// block will be skipped.
+
+		// GIVEN
+		var account1 = new Account(RANDOM_MONEY_AMOUNT);
+		var account2 = new Account(ZERO_MONEY_AMOUNT);
+
+		assertAll("money transaction (dependent)", () -> {
+			// WHEN
+			boolean isTransactionSuccesd = testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT);
+			assertTrue(isTransactionSuccesd);
+
+			// Executed only if the previous assertion is valid.
+			assertAll("Money amount is changed on the accounts",
+					() -> assertEquals(ZERO_MONEY_AMOUNT, account1.getMoneyAmount()),
+					() -> assertEquals(RANDOM_MONEY_AMOUNT, account2.getMoneyAmount()));
+		});
+	}
+
+	@Test
+	void testWithTimeoutExample() {
+		// GIVEN
+		var account1 = new Account(RANDOM_MONEY_AMOUNT);
+		var account2 = new Account(ZERO_MONEY_AMOUNT);
+
+		assertTimeout(Duration.ofSeconds(1), () -> testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT));
+	}
+
+	@Test
+	@Timeout(2)
+	// @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+	void timeoutNotExceededWithResult() {
+		// GIVEN
+		var account1 = new Account(RANDOM_MONEY_AMOUNT);
+		var account2 = new Account(ZERO_MONEY_AMOUNT);
+
+		// The following assertion succeeds, and returns the supplied object.
+		boolean actualResult = assertTimeout(Duration.ofSeconds(1), () -> {
+			return testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT);
+		});
+		assertTrue(actualResult);
+	}
+
+	@Test
+	void testOnlyOnPiatakhaWorkstation() {
+		assumeTrue("truee".equals(System.getenv("IS_ANDRII_PIATAKHA_LAPTOP")),
+				() -> "Aborting this test, because it is running not on " + "laptop of Andrii Piatakha");
+
+		// GIVEN
+		var account1 = new Account(RANDOM_MONEY_AMOUNT);
+		var account2 = new Account(ZERO_MONEY_AMOUNT);
+
+		// The following assertion succeeds, and returns the supplied object.
+		boolean actualResult = assertTimeout(Duration.ofSeconds(1), () -> {
+			return testInstance.transferMoney(account1, account2, RANDOM_MONEY_AMOUNT);
+		});
+		assertTrue(actualResult);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 100, 200, 50, -10 })
+	void parametrizedTestExample(int moneyAmount) {
+		assumeTrue(moneyAmount > 0, () -> "Money amount can't be negative");
+
+		var account1 = new Account(moneyAmount);
+		var account2 = new Account(ZERO_MONEY_AMOUNT);
+
+		assertTrue(testInstance.transferMoney(account1, account2, moneyAmount));
+
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@EmptySource
+	@NullAndEmptySource // die beiden Oberen in einem zusammengefasst. Test wird jeweils 2x ausgeführt
+	void nullAndEmptySources(String text) {
+		assertTrue(text == null || text.trim().isEmpty());
+	}
+
+	@ParameterizedTest
+	@MethodSource("sourceMethod") // die Parameter werden mittels dieser Methode erzeugt (siehe unten)
+	void testMethodSource(String arg) {
+		assertNotNull(arg);
+	}
+
+	// return type can be any type that can be converted to a Stream. For example:
+	// Collection, Iterator, Iterable, IntStream, DoubleStream, LongStream
+	static Stream<String> sourceMethod() {
+		return Stream.of("John", "Walter", "Derek");
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "apple,         1", "banana,        2", "'lemon, lime', 0xF1" })
+	// @Ignore JUnit4
+	// @Disabled
+	// @Disabled("Disabled until Defect #11 will be fixed")
+	void testWithCsvSource(String fruit, int rank) {
+		assertNotNull(fruit);
+		assertNotEquals(0, rank);
 	}
 
 }
